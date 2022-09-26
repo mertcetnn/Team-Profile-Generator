4f01a1a633e3e67a1de69e7dff82a14e1029a421
MertCetin <mert.cetnn@gmail.com>
2022-09-26 10:48:11 -0400

    lib and employe test done

--- __test__/employe.test.js
+++ __test__/employe.test.js
@@ -1,37 +1,47 @@
 
 
+const Employee = require('../lib/employe')
 const employe=require('../lib/employe')
 
-test('employe', ()=>{
-    const i = new employe()
-    expect(typeof(i)).toBe("object")
+test('Creates employe ', ()=>{
+    const employee= new Employee()
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(string));
 
 
 
-}
-
-
-)
+});
 
 
 //passsed test example
 test('employe name',
 ()=>{
-    const i = new employe('bob')
-    expect(i.name).toBe('bob')
+    const employee = new Employee('mert')
+    expect(employee.getName()).toEqual(expect.any(String));
 
 
 
-}
-)
-// couldnt pass example
+});
 
-test('employe name',
-()=>{
-    const i = new employe('bob')
-    expect(i.name).toBe('leks')
+test ('employee id',()=>{
+    const employee =new Employee("123")
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test("employe email",()=>{
+const employee= new Employee('mert.cetnn@gmail.com')
+expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()))
+})
+
+test("role",()=>{
+const employee=new Employee('Employee')
+expect (employee.getRole()).toEqual("Employee")
+
+})
 
 
 
-}
-)
+
+
+
