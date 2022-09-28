a1ef861640f68086bd1db109838d11d10a2ff5c1
MertCetin <mert.cetnn@gmail.com>
2022-09-27 22:26:15 -0400

    prompts done

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const generate = require("./src/generate");
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
+const Employee = require("./lib/employe");
 
 const myTeamArr = [];
 
@@ -77,51 +78,113 @@ const createManager = () => {
 };
 
 const addEmploye = () => {
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "position",
-      message: "Choose employees postition.",
-      chosices: ["Enginer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "What is the company partners name?",
-      validate: (partnerName) => {
-        if (partnerName) {
-          return true;
-        } else {
-          console.log("Dont forget to add partner name");
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "position",
+        message: "Choose employees postition.",
+        chosices: ["Enginer", "Intern"],
       },
-    },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the company partners name?",
+        validate: (partnerName) => {
+          if (partnerName) {
+            return true;
+          } else {
+            console.log("Dont forget to add partner name");
+          }
+        },
+      },
 
-    {
-      type: "input",
-      name: "id",
-      message: "What is the partners ID!",
-      validate: (partnerId) => {
-        if (partnerId) {
-          return true;
-        } else {
-          console.log("Please add Partners ID.");
-        }
+      {
+        type: "input",
+        name: "id",
+        message: "What is the partners ID!",
+        validate: (partnerId) => {
+          if (partnerId) {
+            return true;
+          } else {
+            console.log("Please add Partners ID.");
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Please enter the employee's email.",
-      validate: (email) => {
-        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-        if (valid) {
-          return true;
-        } else {
-          console.log("Please enter an email!");
-          return false;
-        }
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the employee's email.",
+        validate: (email) => {
+          valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+          if (valid) {
+            return true;
+          } else {
+            console.log("Please enter an email!");
+            return false;
+          }
+        },
       },
-    },
-  ]);
+      {
+        type: "input",
+        name: "github",
+        message: "Partner's Github username.",
+        when: (input) => input.position === "Engineer",
+        validate: (partnerGithub) => {
+          if (partnerGithub) {
+            return true;
+          } else {
+            console.log("Please  enter the partners Github!");
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Please enter the intern's school",
+        when: (input) => input.position === "Intern",
+        validate: (internSchool) => {
+          if (internSchool) {
+            return true;
+          } else {
+            console.log("Please enter the intern's school!");
+          }
+        },
+      },
+      {
+        type: "confirm",
+        name: "confirmAddEmp",
+        message: "Would you like to add more team members?",
+        default: false,
+      },
+    ])
+    .then((employeeData) => {
+      let {
+        name,
+        id,
+        email,
+        position,
+        partnerGithub,
+        internSchool,
+        confirmAddEmp,
+      } = employeeData;
+      let employee;
+      if (position === "Engineer") {
+        employee = new Engineer(name, id, email, github);
+
+        console.log(employee);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
+
+        console.log(employee);
+      }
+
+      teamArray.push(employee);
+
+      if (confirmAddEmployee) {
+        return addEmploye(teamArray);
+      } else {
+        return teamArray;
+      }
+    });
 };
